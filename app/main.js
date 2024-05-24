a7cb53f5d77020f71108968f0b3c229944c5e558
FieldMarshallObvious <noahdelangel@gmail.com>
2024-05-24 11:44:06 -0500

    Respond with body

--- app/main.js
+++ app/main.js
@@ -3,17 +3,27 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-valid_paths = ["/"];
+valid_paths = ["", "echo"];
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     parts = String(data).split(" ");
     request = parts[0];
-    path = parts[1];
-    if (request == "GET" && valid_paths.includes(path)) {
-      httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
-      socket.write(httpResponse);
+    path = parts[1].split("/");
+    if (request == "GET" && valid_paths.includes(path[1])) {
+      switch (path[1]) {
+        case "echo":
+          body = path[2];
+          httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n${body}`;
+          socket.write(httpResponse);
+          break;
+
+        default:
+          httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+          socket.write(httpResponse);
+          break;
+      }
     } else {
       const httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
       socket.write(httpResponse);
