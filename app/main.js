3adcf5e9b462085ac7553bbaea3f55c860af9eab
FieldMarshallObvious <noahdelangel@gmail.com>
2024-05-30 11:00:21 -0500

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -3,9 +3,14 @@ const fs = require("fs");
 
 console.log("Logs from your program will appear here!");
 
-const validPaths = ["", "echo", "user-agent", "files"];
+const validGETPaths = ["", "echo", "user-agent", "files"];
+const validPOSTPaths = ["files"];
+
 const HTTP_OK = "HTTP/1.1 200 OK\r\n";
+const HTTP_CREATED = "HTTP/1.1 201 Created\r\n\r\n";
 const HTTP_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n";
+const HTTP_SERVER_ERROR = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+
 const serverPort = 4221;
 const serverHost = "localhost";
 
@@ -19,8 +24,11 @@ const server = net.createServer((socket) => {
       socket.end();
     });
 
-    if (request === "GET" && validPaths.includes(path[1])) {
-      socket.write(buildResponse(path, parts));
+    if (
+      (request === "GET" && validGETPaths.includes(path[1])) ||
+      (request === "POST" && validPOSTPaths.includes(path[1]))
+    ) {
+      socket.write(buildResponse(path, parts, request));
     } else {
       socket.write(HTTP_NOT_FOUND);
     }
@@ -29,7 +37,7 @@ const server = net.createServer((socket) => {
 
 server.listen(serverPort, serverHost);
 
-function buildResponse(path, parts) {
+function buildResponse(path, parts, type) {
   let body = "";
   let responseHeaders = "Content-Type: text/plain\r\n";
   let status = HTTP_NOT_FOUND;
@@ -47,14 +55,27 @@ function buildResponse(path, parts) {
       requested_file = path.length > 2 ? path[2] : "";
       const directory = process.argv[3];
       file_path = directory + "/" + requested_file;
-      console.log("File path:" + file_path);
 
-      try {
-        body = fs.readFileSync(file_path, "utf8").toString();
-        responseHeaders = "Content-Type: application/octet-stream\r\n";
-        status = HTTP_OK;
-      } catch (error) {
-        status = HTTP_NOT_FOUND;
+      if (type === "GET") {
+        try {
+          body = fs.readFileSync(file_path, "utf8").toString();
+          responseHeaders = "Content-Type: application/octet-stream\r\n";
+          status = HTTP_OK;
+        } catch (error) {
+          status = HTTP_NOT_FOUND;
+        }
+      }
+      if (type === "POST") {
+        parts_text = parts.join(" ");
+        let body_seperator = parts_text.indexOf("\r\n\r\n") + 4;
+        let bodyContent = parts_text.substring(body_seperator);
+
+        try {
+          fs.writeFileSync(file_path, bodyContent);
+          status = HTTP_CREATED;
+        } catch (error) {
+          status = HTTP_SERVER_ERROR;
+        }
       }
       break;
 
